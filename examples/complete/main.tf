2166e9ac446cfd4f25b8e441859ab80aa0e21065
liuyuan <709503583@qq.com>
2022-04-24 11:17:10 +0800

    Improves the module examples/complete

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -1,26 +1,45 @@
-resource "alicloud_arms_alert_contact" "default" {
-  alert_contact_name = "example_value"
-  email              = "example_value@aaa.com"
+data "alicloud_cs_managed_kubernetes_clusters" "default" {
 }
 
-data "alicloud_cs_managed_kubernetes_clusters" "default" {
-  name_regex = "tf_Acc"
+resource "alicloud_arms_alert_contact" "default" {
+  alert_contact_name = var.alert_contact_name
+  email              = var.email
 }
 
 module "example" {
-  source                   = "../.."
-  create                   = true
-  alert_name               = "tf_prometheus_alert_name"
-  alert_expression         = "node_memory_MemAvailable_bytes / node_memory_MemTotal_bytes * 100 < 10"
-  alert_message            = "node available memory is less than 10%"
-  prometheus_type          = "Kubernetes component alert"
-  alert_contact_group_name = "tf-arms-contract-group-name"
-  dispatch_rule_name       = "tf-dispatch-rule-name"
-  group_wait_time          = 5
-  group_interval           = 15
-  repeat_interval          = 100
-  notification_name        = "tf-arms-notification"
+  source = "../.."
+
+  #alicloud_arms_alert_contact_group
+  create = true
+
+  alert_contact_group_name = var.alert_contact_group_name
   contact_ids              = [alicloud_arms_alert_contact.default.id]
-  cluster_id               = data.alicloud_cs_managed_kubernetes_clusters.default.id
-  match_expressions        = [{ key = "aliyun_arms_involvedObject_kind", value = "app", operator = "eq" }]
+
+  #alicloud_arms_dispatch_rule
+  dispatch_rule_name = var.dispatch_rule_name
+  dispatch_type      = var.dispatch_type
+  group_wait_time    = var.group_wait_time
+  group_interval     = var.group_interval
+  repeat_interval    = var.repeat_interval
+  grouping_fields    = var.grouping_fields
+  match_expressions = [
+    {
+      key      = "_aliyun_arms_alert_name",
+      value    = "tf-testacc-app",
+      operator = "eq"
+    }
+  ]
+  notify_type       = var.notify_type
+  notification_name = var.notification_name
+  notify_channels   = var.notify_channels
+
+  #alicloud_arms_prometheus_alert_rule
+  alert_name        = "tf-testacc-alert"
+  cluster_id        = data.alicloud_cs_managed_kubernetes_clusters.default.clusters.0.id
+  alert_expression  = "node_memory_MemAvailable_bytes / node_memory_MemTotal_bytes * 100 < 10"
+  alert_message     = "node available memory is less than 10%"
+  alert_duration    = "1"
+  alert_notify_type = var.alert_notify_type
+  prometheus_type   = "alert"
+
 }
